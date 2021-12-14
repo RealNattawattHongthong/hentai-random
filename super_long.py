e944d8de60cb5b297341f63e712958677a8f67ec
Nattawatt Hongthong <73651838+RealNattawattHongthong@users.noreply.github.com>
2021-12-14 17:16:31 +0700

    Create super_long.py

---
+++ super_long.py
@@ -0,0 +1,16 @@
+import random
+import webbrowser
+
+for i in range(5):
+
+    d1 = random.choice([str(random.randint(1, 3)), ''])
+    d2 = str(random.randint(0, 9))
+    d3 = str(random.randint(0, 9))
+    d4 = str(random.randint(0, 9))
+    d5 = str(random.randint(0, 9))
+    d6 = str(random.randint(0, 9))
+
+    sauce = f'https://nhentai.net/g/{d1 + d2 + d3 + d4 + d5 + d6}/'
+
+    webbrowser.open(sauce)
+    print(sauce)
