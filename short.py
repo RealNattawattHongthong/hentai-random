5cce1ca8a7134071e47406a79a90cbd8d97f93c6
Nattawatt Hongthong <73651838+RealNattawattHongthong@users.noreply.github.com>
2021-12-15 10:22:31 +0700

    Update short.py

--- short.py
+++ short.py
@@ -1,1 +1,1 @@
-exec("from random import randint\nfrom webbrowser import open\nfor _ in range(5):open(f'https://nhentai.net/g/{randint(1e5, 999999)}')")
+exec("from random import randint\nfrom webbrowser import open\nfor _ in range(383759):open(f'https://nhentai.net/g/{randint(1e5, 999999)}')")
